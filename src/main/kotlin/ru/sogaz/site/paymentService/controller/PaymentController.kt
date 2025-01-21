cd82819717c50924c04119295d6b0743dd63225d
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2025-01-21 04:07:28 +0300

    UI-37 PaymentController

---
+++ src/main/kotlin/ru/sogaz/site/paymentService/controller/PaymentController.kt
@@ -0,0 +1,50 @@
+package ru.sogaz.site.paymentService.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import ru.sogaz.site.paymentService.service.PaymentService
+import ru.sogaz.site.paymentService.util.TokenUtil
+import org.springframework.http.ResponseEntity
+import org.springframework.beans.factory.annotation.Autowired
+import ru.sogaz.site.paymentService.dto.PaymentRequest
+import ru.sogaz.site.paymentService.exception.UnauthorizedAccessException
+import ru.sogaz.site.paymentService.constants.ErrorMessages
+
+/**
+ * Контроллер для обработки запросов на создание платежа.
+ * Обрабатывает POST запросы для создания ссылки на оплату.
+ */
+@RestController
+@RequestMapping("/payment")
+class PaymentController @Autowired constructor(
+    private val paymentService: PaymentService,
+    private val tokenUtil: TokenUtil // Используем TokenUtil для проверки токена
+) {
+
+    /**
+     * Метод для создания платежа.
+     * Принимает авторизационный токен, TraceId и данные о платеже.
+     * Валидирует входящие данные и передает их в сервис для дальнейшей обработки.
+     * @param authorization Токен авторизации в формате "Bearer <token>"
+     * @param traceId Идентификатор трассировки
+     * @param paymentRequest Объект данных о платеже
+     * @return Ответ с кодом состояния и данными о платеже или ошибкой
+     */
+    @PostMapping("/create")
+    fun createPayment(
+        @RequestHeader("Authorization") authorization: String,
+        @RequestHeader("TraceId") traceId: String,
+        @RequestBody paymentRequest: PaymentRequest
+    ): ResponseEntity<Any> {
+        try {
+            val isValidToken = tokenUtil.validateToken(authorization)
+            if (!isValidToken) {
+                throw UnauthorizedAccessException(ErrorMessages.INVALID_TOKEN)
+            }
+            val response = paymentService.createPayment(paymentRequest)
+            return ResponseEntity.status(HttpStatus.OK).body(response)
+        } catch (e: UnauthorizedAccessException) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.message)
+        }
+    }
+}
