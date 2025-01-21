c153965298dfb6c7014219bb8786ed98f3b9b76f
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2025-01-21 03:58:18 +0300

    UI-37 PaymentService

---
+++ src/main/kotlin/ru/sogaz/site/paymentService/service/PaymentService.kt
@@ -0,0 +1,41 @@
+package ru.sogaz.site.paymentService.service
+
+import ru.sogaz.site.paymentService.dto.PaymentRequest
+import ru.sogaz.site.paymentService.validation.PaymentValidator
+import org.springframework.stereotype.Service
+
+/**
+ * Сервис для обработки платежей.
+ * Включает в себя валидацию данных и создание записи о платеже.
+ */
+@Service
+class PaymentService(private val paymentValidator: PaymentValidator) {
+
+    /**
+     * Метод для создания платежа.
+     * Проверяет данные о платеже, валидирует их и создает запись о платеже.
+     * @param paymentRequest Данные о платеже
+     * @throws Exception Если данные невалидны или произошла ошибка при сохранении
+     * @return Объект Payment, содержащий информацию о платежном запросе
+     */
+    fun createPayment(paymentRequest: PaymentRequest) {
+
+        validatePaymentData(paymentRequest)
+    }
+
+    /**
+     * Метод для валидации данных платежа.
+     * Проверяет все обязательные поля на корректность.
+     * @param paymentRequest Данные о платеже
+     * @throws Exception Если какие-либо поля невалидны
+     */
+    private fun validatePaymentData(paymentRequest: PaymentRequest) {
+        paymentValidator.validateEmail(paymentRequest.recipientEmail)
+        paymentValidator.validatePhone(paymentRequest.recipientPhone)
+        paymentValidator.validatePolicyholder(paymentRequest.policyholder)
+        paymentValidator.validateExternalSystemCode(paymentRequest.externalSystemCode)
+        paymentValidator.validatePaymentEndDate(paymentRequest.paymentEndDate)
+        paymentValidator.validateDateFormat(paymentRequest.paymentEndDate)
+        paymentValidator.validateBank(paymentRequest.bank)
+    }
+}
