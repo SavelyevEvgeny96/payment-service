560c9b7e510d144d18acef7ac852a60c5ce7567a
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2025-08-20 14:28:40 +0300

    SITE-1049 доработки после ревью

--- src/main/kotlin/ru/sogaz/site/paymentService/entity/SubOrder.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/entity/SubOrder.kt
@@ -7,7 +7,10 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.PrePersist
+import jakarta.persistence.PreUpdate
 import jakarta.persistence.Table
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "sub_orders")
@@ -41,4 +44,19 @@ data class SubOrder(
     var contractNumber: String? = "",
     @Column(name = "premium_amount")
     var premiumAmount: String? = "",
-)
+    @Column(name = "create_date", updatable = false)
+    var createDate: LocalDateTime? = null,
+    @Column(name = "update_date")
+    var updateDate: LocalDateTime? = null,
+) {
+    @PrePersist
+    fun prePersist() {
+        createDate = LocalDateTime.now()
+        updateDate = LocalDateTime.now()
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        updateDate = LocalDateTime.now()
+    }
+}
