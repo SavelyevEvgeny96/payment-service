fbca63564f6ce20ea21a7817a30c9cbaecfbadf2
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2025-08-22 01:37:58 +0300

    SITE-993 Добавил Новую интеграцию банк Россия

---
+++ src/main/kotlin/ru/sogaz/site/paymentService/service/AkbBankIntegrationService.kt
@@ -0,0 +1,19 @@
+package ru.sogaz.site.paymentService.service
+
+import org.springframework.http.ResponseEntity
+import ru.sogaz.site.paymentService.dto.DataPay
+import ru.sogaz.site.paymentService.entity.Order
+import ru.sogaz.site.paymentService.entity.SubOrder
+import ru.sogaz.siter.models.resonses.Response
+
+interface AkbBankIntegrationService {
+    fun initiateAKBPayment(
+        urlToReturn: String?,
+        urlToReturnF: String?,
+        orderId: String,
+        paymentId: Long?,
+        premiumAmount: String?,
+        order: Order,
+        subOrder: SubOrder,
+    ): ResponseEntity<Response<DataPay>>
+}
---
+++ src/main/kotlin/ru/sogaz/site/paymentService/service/impl/AkbBankIntegrationServiceImpl.kt
@@ -0,0 +1,32 @@
+package ru.sogaz.site.paymentService.service.impl
+
+import org.springframework.http.ResponseEntity
+import ru.sogaz.site.paymentService.dto.DataPay
+import ru.sogaz.site.paymentService.entity.Order
+import ru.sogaz.site.paymentService.entity.SubOrder
+import ru.sogaz.site.paymentService.loggerFor
+import ru.sogaz.site.paymentService.service.AkbBankIntegrationService
+import ru.sogaz.siter.models.resonses.Response
+
+class AkbBankIntegrationServiceImpl() : AkbBankIntegrationService {
+
+    companion object {
+        const val MESSAGE_INFO_START_AKB_PAYMENT =
+            ">>> СТАРТ метода оплата картой Банк Россия для платежа с payment_id: "
+        const val MESSAGE_INFO_END_AKB_PAYMENT = "<<< КОНЕЦ метода оплата картой Банк Россия для платежа с payment_id: "
+    }
+
+    private val logger = loggerFor(javaClass)
+    override fun initiateAKBPayment(
+        urlToReturn: String?,
+        urlToReturnF: String?,
+        orderId: String,
+        paymentId: Long?,
+        premiumAmount: String?,
+        order: Order,
+        subOrder: SubOrder
+    ): ResponseEntity<Response<DataPay>> {
+        logger.info("$MESSAGE_INFO_START_AKB_PAYMENT $paymentId")
+
+    }
+}
