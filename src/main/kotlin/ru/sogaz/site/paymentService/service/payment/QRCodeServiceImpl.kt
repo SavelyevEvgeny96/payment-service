716620099e5a3117ad3dab8ba3e8d43f171696e7
Соколов Данила Андреевич <sokolov.danila@sogaz.ru>
2025-09-24 12:10:28 +0000

    SITE-1128: Добавление url для обращения к qr-generator-service

--- src/main/kotlin/ru/sogaz/site/paymentService/service/payment/QRCodeServiceImpl.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/service/payment/QRCodeServiceImpl.kt
@@ -3,7 +3,6 @@ package ru.sogaz.site.paymentService.service.payment
 import ru.sogaz.site.paymentService.dto.data.FileQR
 import ru.sogaz.site.paymentService.dto.response.GPBQRImageResponse
 import ru.sogaz.site.paymentService.entity.Payment
-import ru.sogaz.site.paymentService.enums.BankEnum
 import ru.sogaz.site.paymentService.enums.MediaTypeValue
 import ru.sogaz.site.paymentService.loggerFor
 import ru.sogaz.site.paymentService.service.QRCodeService
@@ -17,10 +16,6 @@ class QRCodeServiceImpl(
     private val qrCodeControllerApi: QrCodeControllerApi,
     private val bankIntegrationFactoryService: BankIntegrationFactoryService,
 ) : QRCodeService {
-    companion object {
-        const val GPB_INSTEAD_OF_AKB_FOR_QR = "GPB was selected to generate QR code instead of AKB"
-    }
-
     private val logger = loggerFor(javaClass)
 
     override fun generateQRCode(
@@ -44,20 +39,13 @@ class QRCodeServiceImpl(
 
     override fun requestFromBank(payment: Payment): Optional<FileQR> =
         try {
-            resolveBankForQRRequest(payment.bank)
-                .run { bankIntegrationFactoryService.getInstanceByBank(this) }
+            bankIntegrationFactoryService.getInstanceByBank(payment.bank)
                 .run { this.getQRCodeImageData(payment) }
                 .run { makeFileQREntity(this) }
                 .run { Optional.of(this) }
         } catch (ex: Exception) {
             logger.error(ex.message)
             Optional.empty<FileQR>()
-        }
-
-    private fun resolveBankForQRRequest(bank: BankEnum?): BankEnum =
-        when (bank) {
-            BankEnum.AKB_RUS -> BankEnum.GPB.also { logger.info(GPB_INSTEAD_OF_AKB_FOR_QR) }
-            else -> BankEnum.GPB
         }
 
     private fun makeFileQREntity(responseQRCodeData: ResponseQRCodeData) =
