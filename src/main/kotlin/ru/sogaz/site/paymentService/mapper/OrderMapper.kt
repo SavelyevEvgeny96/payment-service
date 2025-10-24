df774c0a4377b0947d373dc5078a0e458c901eb0
Максим Евгеньевич Блажин <Blazhin.Maxim@sogaz.ru>
2025-10-24 16:20:11 +0300

    SITE-1534: Добавление errorCode для ForbiddenBusinessException. Исправление expression в orderMapper

--- src/main/kotlin/ru/sogaz/site/paymentService/mapper/OrderMapper.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/mapper/OrderMapper.kt
@@ -12,13 +12,17 @@ import ru.sogaz.site.paymentService.enums.OrderStatus
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.util.Objects
 
-@Mapper(imports = [OrderStatus::class])
+@Mapper(imports = [OrderStatus::class, Objects::class])
 interface OrderMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(
         target = "status",
-        expression = "java(updatePaymentInvoiceRequest.getCancelCheck() ? OrderStatus.CANCELED : order.getStatus())",
+        expression =
+            "java(!Objects.isNull(updatePaymentInvoiceRequest.getCancelCheck())" +
+                " && updatePaymentInvoiceRequest.getCancelCheck()" +
+                " ? OrderStatus.CANCELED : order.getStatus())",
     )
     @Mapping(
         target = "paymentEndDate",
