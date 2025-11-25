3c68aa7be4a4297fa57098fd4ffc3695a84ce0e6
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2025-11-25 15:14:39 +0300

    SITE-1809 ИСПРАВИЛ ЛОГИКУ

--- src/main/kotlin/ru/sogaz/site/paymentService/mapper/order/OrderPayloadMapper.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/mapper/order/OrderPayloadMapper.kt
@@ -3,21 +3,23 @@ package ru.sogaz.site.paymentService.mapper.order
 import org.mapstruct.BeanMapping
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.Named
+import ru.sogaz.site.paymentService.dto.rabbit.MetaInfoOrder
 import ru.sogaz.site.paymentService.dto.rabbit.OrderPayloadDto
 import ru.sogaz.site.paymentService.dto.request.OrderRequest
 
 @Mapper(componentModel = "spring")
-interface OrderPayloadMapper {
+abstract class OrderPayloadMapper {
     @BeanMapping(
-        ignoreUnmappedSourceProperties = [
-            "metaInfo",
-            "paymentType",
-        ],
+        ignoreUnmappedSourceProperties = ["paymentType"],
     )
     @Mapping(target = "orders", source = "subOrders")
     @Mapping(target = "urlToReturn", ignore = true)
     @Mapping(target = "urlToDecline", ignore = true)
     @Mapping(target = "saveCard", ignore = true)
-    @Mapping(target = "clientId", ignore = true)
-    fun toRequest(payload: OrderPayloadDto): OrderRequest
+    @Mapping(target = "clientId", source = "metaInfo", qualifiedByName = ["mapClientId"])
+    abstract fun toRequest(payload: OrderPayloadDto): OrderRequest
+
+    @Named("mapClientId")
+    open fun mapClientId(metaInfo: List<MetaInfoOrder>): String? = metaInfo.firstOrNull()?.author
 }
