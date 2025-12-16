766b49d5e28dc5de1acf17070b0d8cdff00b4d0d
Максим Евгеньевич Блажин <Blazhin.Maxim@sogaz.ru>
2025-12-16 21:48:48 +0300

    SITE-2135: Добавление named метода для маппинга extended code

--- src/main/kotlin/ru/sogaz/site/paymentService/mapper/payment/BankPaymentDetailsMapper.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/mapper/payment/BankPaymentDetailsMapper.kt
@@ -2,21 +2,23 @@ package ru.sogaz.site.paymentService.mapper.payment
 
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.Named
 import ru.sogaz.site.paymentService.dto.data.BankPaymentDetails
 import ru.sogaz.site.paymentService.dto.response.AkbOrderStatusResponse
 import ru.sogaz.site.paymentService.dto.response.bank.GpbCardPaymentStatusResponse
 import ru.sogaz.site.paymentService.dto.response.bank.GpbQrResult
 import ru.sogaz.site.paymentService.enums.AkbPaymentStatusEnum
+import ru.sogaz.site.paymentService.enums.PaymentExtendedCodeMessage
 import ru.sogaz.site.paymentService.enums.PaymentStatusEnum
 
 @Mapper(
     componentModel = "spring",
     uses = [CardDetailsMapper::class, PaymentStatusMapper::class],
-    imports = [PaymentStatusEnum::class],
+    imports = [PaymentStatusEnum::class, PaymentExtendedCodeMessage::class],
 )
 interface BankPaymentDetailsMapper {
     @Mapping(target = "status", source = "result.status", defaultValue = "WAIT")
-    @Mapping(target = "extendedCode", source = "result.extendedCode")
+    @Mapping(target = "extendedCode", source = "result.extendedCode", qualifiedByName = ["mapExtendedCode"])
     @Mapping(target = "cardDetails", source = "gpbCardDetails")
     fun convert(response: GpbCardPaymentStatusResponse): BankPaymentDetails
 
@@ -26,4 +28,10 @@ interface BankPaymentDetailsMapper {
         response: AkbOrderStatusResponse,
         status: AkbPaymentStatusEnum,
     ): BankPaymentDetails
+
+    companion object {
+        @JvmStatic
+        @Named("mapExtendedCode")
+        fun mapExtendedCode(code: String?): String? = PaymentExtendedCodeMessage.fromCode(code)
+    }
 }
