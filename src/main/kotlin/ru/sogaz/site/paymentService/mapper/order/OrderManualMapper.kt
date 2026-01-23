2ab5f073352d5e02b4ef9492b49600b12c2bba37
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2026-01-23 12:52:02 +0300

    Правка в тест срочно для подписок

--- src/main/kotlin/ru/sogaz/site/paymentService/mapper/order/OrderManualMapper.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/mapper/order/OrderManualMapper.kt
@@ -3,6 +3,7 @@ package ru.sogaz.site.paymentService.mapper.order
 import org.springframework.stereotype.Component
 import ru.sogaz.site.paymentService.dto.request.OrderRequest
 import ru.sogaz.site.paymentService.entity.Order
+import java.math.RoundingMode
 
 @Component
 class OrderManualMapper(
@@ -10,11 +11,17 @@ class OrderManualMapper(
 ) {
     fun mapRequestToOrder(orderRequest: OrderRequest): Order {
         val order = orderMapper.fromRequestDto(orderRequest)
-
         val subOrders =
             orderRequest.orders
                 .map(orderMapper::fromRequestDto)
-                .onEach { it.order = order }
+                .onEach { subOrder ->
+                    subOrder.order = order
+                    subOrder.premiumAmount =
+                        subOrder.premiumAmount
+                            ?.toBigDecimal()
+                            ?.setScale(2, RoundingMode.HALF_UP)
+                            ?.toPlainString()
+                }
 
         order.subOrders.addAll(subOrders)
         return order
