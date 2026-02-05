eca48c4b126840c1119a1471e75c77d59acac6d8
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2026-02-06 00:41:24 +0300

    Фикс на тест

--- src/main/resources/db.changelog/v2.2.3/2026-02-05/DATA/add_system.sql
+++ src/main/resources/db.changelog/v2.2.3/2026-02-05/DATA/add_system.sql
@@ -1,3 +1,3 @@
 -- === Редактирование ТАБЛИЦЫ client_systems ===
 
-INSERT INTO client_systems(external_system_code,external_system_name)VALUES('lk-sogaz-client','ДМС');
+INSERT INTO client_systems(external_system_code,external_system_name)VALUES('fldms-client','ДМС');
