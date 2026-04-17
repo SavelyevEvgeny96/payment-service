38479ea70ee783a28177689d04dbbc6e917d6a72
Соколов Данила Андреевич <sokolov.danila@sogaz.ru>
2026-04-17 14:21:27 +0000

    [SITE-2325] Сохранения данных для отчетов
    # Conflicts:
    #	src/main/kotlin/ru/sogaz/site/paymentService/mapper/v2/operation/RefundOperationRequestMapper.kt
    #	src/main/kotlin/ru/sogaz/site/paymentService/model/v2/entity/IdempotentOrderOperation.kt
    #	src/main/kotlin/ru/sogaz/site/paymentService/service/callback/MockSignatureVerifierImpl.kt

--- src/main/kotlin/ru/sogaz/site/paymentService/mapper/v2/event/CompletedOperationMapper.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/mapper/v2/event/CompletedOperationMapper.kt
@@ -4,9 +4,10 @@ import org.mapstruct.Mapper
 import org.mapstruct.Mapping
 import ru.sogaz.site.paymentService.model.v2.bank.response.BankOperationDetails
 import ru.sogaz.site.paymentService.model.v2.entity.IdempotentOrderOperation
+import ru.sogaz.site.paymentService.model.v2.enums.OperationState
 import ru.sogaz.site.paymentService.model.v2.event.CompletedOperationEvent
 
-@Mapper
+@Mapper(imports = [OperationState::class])
 interface CompletedOperationMapper {
     @Mapping(target = "paymentId", source = "operation.id")
     @Mapping(target = "orderId", source = "operation.idempotentOrder.id")
@@ -14,6 +15,12 @@ interface CompletedOperationMapper {
     @Mapping(target = "totalAmount", source = "operation.premiumAmount")
     @Mapping(target = "status", source = "operationDetails.state")
     @Mapping(target = "card", source = "operationDetails.cardDetails")
+    @Mapping(target = "errorText", source = "operationDetails.errorText")
+    @Mapping(
+        target = "externalErrorCode",
+        source = "operationDetails.extendedCode",
+        conditionExpression = "java( operationDetails.getState() == OperationState.FAIL )",
+    )
     fun completedOperationEvent(
         operation: IdempotentOrderOperation,
         operationDetails: BankOperationDetails,
