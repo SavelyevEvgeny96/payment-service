045fdcd823c4574ecb630aed98c1a8aee53701fb
Evgeny Savelev <Savelev.Evgeny.V@sogaz.ru>
2026-05-13 01:58:17 +0300

    RNA-3214 добавил при обращении в ГПБ

--- src/main/kotlin/ru/sogaz/site/paymentService/dto/request/GPBQRImageRequest.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/dto/request/GPBQRImageRequest.kt
@@ -1,7 +1,10 @@
 package ru.sogaz.site.paymentService.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class GPBQRImageRequest(
-    val qrcId: String,
+    @param:JsonProperty("qrcId")
+    val qrId: String,
     val width: Int = 300,
     val height: Int = 300,
 )
