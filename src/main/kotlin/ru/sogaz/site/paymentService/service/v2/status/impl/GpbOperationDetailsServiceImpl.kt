56a5e0425b93e6898ec0fd5bd9e6d22fca8cbb55
Evgeny Savelev <Savelev.Evgeny.V@sogaz.ru>
2026-05-12 13:10:53 +0300

    SITE-3018 ВОЗВРАТЫ
    # Conflicts:
    #	src/main/kotlin/ru/sogaz/site/paymentService/service/v2/status/impl/GpbOperationDetailsServiceImpl.kt

--- src/main/kotlin/ru/sogaz/site/paymentService/service/v2/status/impl/GpbOperationDetailsServiceImpl.kt
+++ src/main/kotlin/ru/sogaz/site/paymentService/service/v2/status/impl/GpbOperationDetailsServiceImpl.kt
@@ -21,7 +21,7 @@ class GpbOperationDetailsServiceImpl(
         when (idempotentOrderOperation.operationType) {
             OperationType.PAY -> getPayOperationDetails(idempotentOrderOperation)
             OperationType.RECURRENT -> getPayOperationDetails(idempotentOrderOperation)
-            OperationType.CARD_REGISTRATION -> TODO()
+            OperationType.CARD_REGISTRATION -> getPayOperationDetails(idempotentOrderOperation)
             OperationType.REFUND -> TODO()
         }
 
