c95c5682a88f3347d402e92e840a2e1e63a6ec70
Evgeny Savelev <Savelev.Evgeny.V@sogaz.ru>
2026-05-15 11:31:54 +0300

    пРАВКА Свагера

---
+++ src/main/kotlin/ru/sogaz/site/paymentService/dto/data/PayItemsSwaggerSchema.kt
@@ -0,0 +1,21 @@
+package ru.sogaz.site.paymentService.dto.data
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Список показов")
+data class PayItemsSwaggerSchema(
+
+    @field:Schema(
+        description = "Наименование подзаказа",
+        example = "SF-000011950 от 15.09.2026",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    val param1: String,
+
+    @field:Schema(
+        description = "Наименование подзаказа",
+        example = "SF-000011951 от 15.09.2026",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    val param2: String
+)
